845ba5be796f9cf32319d74987260be136d586f8
Marcela Hadassa <marcelahadassa.pereira@gmail.com>
2024-10-08 19:20:40 -0300

    feat: create presentation component

---
+++ src/app/layouts/presentation/presentation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './presentation.component.html'
+})
+export class PresentationComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
-
+import { PresentationComponent } from '../../layouts/presentation/presentation.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterModule, 
-    HeaderComponent],
+    HeaderComponent,
+    PresentationComponent
+  ],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
